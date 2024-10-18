bea14d47cb4cec15d1f32e4fd0a7d64c298f34b1
BucketXv <BucketXv@localhost>
2024-10-18 00:29:57 -0500

    Save

--- setup/configure_server.sh
+++ setup/configure_server.sh
@@ -1,7 +1,10 @@
+cp /users/BucketXv/.ssh/id_ed25519 /root/.ssh/id_ed25519
 while read -r -u10 osd_ip hostname
 do
     scp ~/.ssh/id_ed25519 "$osd_ip:~/.ssh/"
     sudo cat /etc/ceph/ceph.pub | ssh $osd_ip "sudo cat >> ~/.ssh/authorized_keys"
     ssh $osd_ip "sudo apt update && sudo apt install containerd && sudo apt install docker.io"
-    sudo ceph orch host add $hostname $osd_ip
+    sudo -i
+    ceph orch host add $hostname $osd_ip
+    exit
 done 10< ip_addrs_server.txt
