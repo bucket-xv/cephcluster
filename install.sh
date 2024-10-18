c20dd94dd1c765639d5979b5b756aa2125149e3b
Chenhao Xu <xv_chen_hao@163.com>
2024-10-18 13:14:51 +0800

    store

--- install.sh
+++ install.sh
@@ -1,38 +1,10 @@
-# for monitor node first
-sudo -i
-apt update
-# mkfs.ext4 /dev/sda4
-# mkdir /mnt/sda4
-# mount --bind /var/lib/ceph /mnt/sda4
-mount -t auto -v /dev/sda4 /var/lib/ceph
-# mv /mnt/sda4/* /var/lib/ceph
-umount /mnt/sda4
-
-sudo apt update
-CEPH_RELEASE=18.2.4
-curl --silent --remote-name --location https://download.ceph.com/rpm-${CEPH_RELEASE}/el9/noarch/cephadm
-chmod +x cephadm
-sudo ./cephadm add-repo --release Reef
-sudo ./cephadm install
-sudo apt install python3.10
-sudo cephadm add-repo --release reef
-sudo cephadm install ceph-common
-sudo cephadm bootstrap --mon-ip 128.105.145.216 --cleanup-on-failure --allow-fqdn-hostname
-exit
-
-# install
-
 
 # for osd nodes
-OSD_IP=10.10.1.3
-cat /etc/ceph/ceph.pub | ssh $OSD_IP "sudo cat >> ~/.ssh/authorized_keys"
-ssh $OSD_IP "apt update && apt install docker.io"
-ceph orch host add server-2.bucketxv-221991.dstore-pg0.wisc.cloudlab.us $OSD_IP
+
 
 # for clients
 sudo apt update
-CEPH_RELEASE=18.2.4
-curl --silent --remote-name --location https://download.ceph.com/rpm-${CEPH_RELEASE}/el9/noarch/cephadm
+CEPH_RELEASE=18.2.4 curl --silent --remote-name --location https://download.ceph.com/rpm-${CEPH_RELEASE}/el9/noarch/cephadm
 chmod +x cephadm
 sudo ./cephadm add-repo --release Reef
 sudo ./cephadm install
--- setup/configure_server.sh
+++ setup/configure_server.sh
@@ -1,40 +1,7 @@
-#!/bin/bash
-
-# This is a script for Ubuntu 18
-sudo apt update -y
-sudo apt-get install gcc g++ make cmake git autogen autoconf automake yasm nasm libtool libboost-all-dev -y
-sudo apt-get install -y help2man
-
-cd ~/dRAID4Docker/
-git submodule update --init --recursive
-
-# These are to install the isa-l library
-cd submodules/isa-l
-./autogen.sh
-./configure
-make
-sudo make install
-cd ../..
-
-# These are to install the spdk library
-cd ../submodules/spdk
-git submodule update --init
-sudo ./scripts/pkgdep.sh
-./configure
-make
-cd ../..
-
-# install dependencies of Linux RAID
-# sudo apt update -y
-# sudo apt install nvme-cli -y
-# sudo apt install mdadm -y
-# sudo apt install libaio-dev -y
-# sudo modprobe nvme-rdma
-# sudo mkdir /raid
-# install FIO
-# git clone https://github.com/axboe/fio
-# cd fio
-# make
-# sudo make install
-# sudo ldconfig
-cd ..
+while read -r -u10 osd_ip hostname
+do
+    scp ~/.ssh/id_ed25519 "$osd_ip:~/.ssh/"
+    sudo cat /etc/ceph/ceph.pub | ssh $osd_ip "sudo cat >> ~/.ssh/authorized_keys"
+    ssh $osd_ip "sudo apt update && sudo apt install containerd && sudo apt install docker.io"
+    sudo ceph orch host add $hostname $osd_ip
+done 10< ip_addrs_server.txt
