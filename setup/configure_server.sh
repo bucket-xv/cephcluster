4555e49d85125811e628ddf7fa9469ff4d489d75
Chenhao Xu <xv_chen_hao@163.com>
2024-10-18 19:30:34 +0800

    Fix

--- setup/configure_server.sh
+++ setup/configure_server.sh
@@ -2,7 +2,8 @@ while read -r -u10 osd_ip hostname
 do
     scp ~/.ssh/id_ed25519 "$osd_ip:~/.ssh/"
     sudo cat /etc/ceph/ceph.pub | ssh $osd_ip "sudo cat >> ~/.ssh/authorized_keys"
-    ssh $osd_ip "sudo apt update && sudo apt install containerd && sudo apt install docker.io"
+    ssh $osd_ip "sudo mkdir -p /var/lib/ceph && sudo mkfs.ext4 /dev/sda4 && sudo mount -t auto -v /dev/sda4 /var/lib/ceph"
+    ssh $osd_ip "sudo apt update && sudo apt install containerd -y && sudo apt install docker.io -y"
     sudo ssh-copy-id -f -i /etc/ceph/ceph.pub root@$osd_ip
     sudo ceph orch host add $hostname $osd_ip
 done 10< ip_addrs_server.txt
