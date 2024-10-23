f4c649e58de82962f36a88cfd596e4339509d35d
Chenhao Xu <xv_chen_hao@163.com>
2024-10-23 16:00:40 +0800

    Fix

--- setup/configure_host.sh
+++ setup/configure_host.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
+# mount sda4 to /var/lib/ceph
 sudo apt update
 sudo mkdir -p /var/lib/ceph
 sudo mkfs.ext4 /dev/sda4
-# mkdir /mnt/sda4
-# mount --bind /var/lib/ceph /mnt/sda4
 sudo mount -t auto -v /dev/sda4 /var/lib/ceph
-# mv /mnt/sda4/* /var/lib/ceph
-# sudo umount /mnt/sda4
 
+# install cephadm and ceph-common
 cd ~
-sudo apt install docker.io
-sudo apt install software-properties-common -y
+sudo apt install docker.io software-properties-common -y
 sudo add-apt-repository ppa:deadsnakes/ppa -y
-sudo apt install python3.10
+sudo apt install python3.10 -y
 CEPH_RELEASE=18.2.4 
 sudo curl --silent --remote-name --location https://download.ceph.com/rpm-${CEPH_RELEASE}/el9/noarch/cephadm
 sudo chmod +x cephadm
@@ -21,6 +18,10 @@ sudo ./cephadm add-repo --release reef
 sudo ./cephadm install
 sudo cephadm add-repo --release reef
 sudo cephadm install ceph-common
+sudo cephadm install ceph-base
+
+# cephadm bootstrap
+cd ~/cephcluster/setup
 host=$(head -n 1 ip_addr_host.txt)
 sudo cephadm bootstrap --mon-ip $host --cleanup-on-failure --allow-fqdn-hostname
 sudo ceph orch apply osd --all-available-devices
